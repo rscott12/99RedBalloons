adaf627810f036a41346738db351a44f256d9bbd
Raymond Scott <genji@genji.home>
2014-10-02 23:56:46 -0500

    with func

--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -9,49 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
 
     @IBOutlet weak var myImageView: UIImageView!
-    
     
     @IBOutlet weak var balloonLabel: UILabel!
     
     var currentIndex = 1
     var myBalloons:[Balloon] = []
     var randomNumber = 0
-    var myBalloon = Balloon()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        
-        for (var i = 1; i < 100; i++)
-        {
-            myBalloon = Balloon()
-            myBalloon.number = i
-            randomNumber = Int(arc4random_uniform(UInt32(3)))
-            
-            switch randomNumber {
-            case 0:
-                myBalloon.image = UIImage(named: "RedBalloon1.jpg")
-            case 1:
-                myBalloon.image = UIImage(named: "RedBalloon2.jpg")
-            case 2:
-                myBalloon.image = UIImage(named: "RedBalloon3.jpg")
-            case 3:
-                myBalloon.image = UIImage(named: "RedBalloon4.jpg")
-            default:
-                myBalloon.image = UIImage(named: "BerlinTVTower.jpg")
-            }
-            myBalloons.append(myBalloon)
-        }
+        // Do any additional setup after loading the view, typically from a nib
+        loadBalloons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadBalloons ()
+    {
+        for (var i = 1; i < 100; i++)
+        {
+            var myBalloon = Balloon()
+            myBalloon.number = i
+            randomNumber = Int(arc4random_uniform(UInt32(4)))
+            
+            switch randomNumber {
+            case 1:
+                myBalloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2:
+                myBalloon.image = UIImage(named: "RedBalloon2.jpg")
+            case 3:
+                myBalloon.image = UIImage(named: "RedBalloon3.jpg")
+            default:
+                myBalloon.image = UIImage(named: "RedBalloon4.jpg")
+            }
+            myBalloons.append(myBalloon)
+        }
     }
     
     
@@ -68,12 +65,7 @@ class ViewController: UIViewController {
         
         var balloon = myBalloons[randomIndex]
         
-        balloonLabel.text = "\(balloon.number) Balloons"
+        balloonLabel.text = "\(balloon.number) Balloon"
         myImageView.image = balloon.image
-        
     }
-    
-
-
 }
-
